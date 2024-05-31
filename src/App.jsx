4ad7a19879cb57f745e1f612168657c87078d9d3
Aldana Escobar <aldana.escobarnandez@gmail.com>
2024-05-30 22:32:54 -0300

    Add Routers in App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from "./components/cards/ItemListContainer";
 import NavBar from "./components/navBar/navBar";
 import "./styles/homePage.css";
+import ItemDetailContainer from "./components/cards/itemDetailContainer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div className="app-container">
         <NavBar />
-        <ItemListContainer />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+          <Route path="/*" />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
