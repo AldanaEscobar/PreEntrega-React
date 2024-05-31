3d4bfb3b07bc4f17f824086e5ea50d63638f15e0
Aldana Escobar <aldana.escobarnandez@gmail.com>
2024-05-30 22:20:54 -0300

    Modify itemDetailContainer

--- src/components/cards/itemDetailContainer.jsx
+++ src/components/cards/itemDetailContainer.jsx
@@ -1,46 +1,34 @@
 import { useEffect, useState } from "react";
-import productsData from "../../data/products.json";
+import { useParams } from "react-router-dom";
 import "../../styles/itemlist.css";
 import "../../styles/counter.css";
+import productsData from "../../data/products.json";
 
-const ItemDetailContainer = ({ selectedProductId }) => {
-  const [selectedProduct, setSelectedProduct] = useState(null);
-  // const [products, setProducts] = useState([]);
-
-  const showProducts = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(productsData);
-      }, 2000);
-    });
-  };
+const ItemDetailContainer = () => {
+  let { itemId } = useParams();
+  let [product, setProduct] = useState(undefined);
 
   useEffect(() => {
-    showProducts().then((data) => {
-      const product = data.find((p) => p.id === selectedProductId);
-      setSelectedProduct(product);
-    });
-  }, [selectedProductId]);
-  console.log('SelectedProd:', selectedProduct)
+    setProduct(productsData.find((prod) => prod.id === parseInt(itemId)));
+  }, [itemId]);
 
   return (
     <>
-      {selectedProduct ? (
+      {product ? (
         <div className="detail-container">
-          <h2>{selectedProduct.name}</h2>
-          <p>{selectedProduct.longdescription}</p>
-          <p>{selectedProduct.price} USD</p>
-          {selectedProduct.image && <img src={selectedProduct.image} alt={selectedProduct.name} />}
+          <h2>{product.name}</h2>
+          <p>{product.longdescription}</p>
+          <p>{product.price} USD</p>
+          {product.image && <img src={product.image} alt={product.name} />}
         </div>
       ) : (
-        <div></div>
-        // <div className="load-container">
-        //   <div className="loader">
-        //     <div className="line"></div>
-        //     <div className="line"></div>
-        //     <div className="line"></div>
-        //   </div>
-        // </div>
+        <div className="load-container">
+          <div className="loader">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </div>
       )}
     </>
   );
