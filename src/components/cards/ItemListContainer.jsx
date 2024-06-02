ce510d80ea7f046e07218c59d549d03aac7270a9
Aldana Escobar <aldana.escobarnandez@gmail.com>
2024-06-02 12:13:55 -0300

    change title

--- src/components/cards/ItemListContainer.jsx
+++ src/components/cards/ItemListContainer.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ItemListContainer() {
+  const [title, setTitle] = useState("");
   let [products, setProducts] = useState([]);
   const category = useParams().categoryId;
 
@@ -22,17 +23,28 @@ function ItemListContainer() {
       .then((res) => {
         if(category) {
           setProducts(res.filter((product) => product.category === category));
+          setTitle(formatTitle(category));
         } else {
         setProducts(res);}
+        // setTitle("Conoce todas nuestras guias");
       })
       .catch((err) => {
         console.error(err);
       });
   }, [category]);
 
+
+
+  const formatTitle = (str) => {
+    if (str === "nomadedigital") {
+      return "Nomade digital";
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
   return (
     <div className="body-container">
-      <h2 className='title'>Guias:</h2>
+      <h2 className='title'> {category ? "Guia: " + title : "Conoce todas nuestras guias"}</h2>
       <div className='cards'>
       <ItemList products={products}/>
       </div>
