8e9ed5d267f02edccbd32bb868ea46a06e5ca1ba
Aldana Escobar <aldana.escobarnandez@gmail.com>
2024-06-18 20:15:23 -0300

    modify

--- src/components/cards/ItemListContainer.jsx
+++ src/components/cards/ItemListContainer.jsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from '../../firebase/config.js';
 import '../../styles/ItemListContainer.css';
 import ItemList from './itemList';
 import '../../styles/itemlist.css';
 
 function ItemListContainer() {
-  // const [title, setTitle] = useState("");
   const [products, setProducts] = useState([]);
-  const category = useParams().categoryId;
+  const { categoryId } = useParams();
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const productsCollection = collection(db, "guias");
-      const productsSnapshot = await getDocs(productsCollection);
+      let productsQuery;
+
+      if (categoryId) {
+        productsQuery = query(
+          collection(db, "guias"),
+          where("category", "==", categoryId)
+        );
+      } else {
+        productsQuery = collection(db, "guias");
+      }
+
+      const productsSnapshot = await getDocs(productsQuery);
       const productsList = productsSnapshot.docs.map(doc => ({
         ...doc.data(),
         id: doc.id
@@ -23,7 +32,7 @@ function ItemListContainer() {
     };
 
     fetchProducts();
-  }, [category]);
+  }, [categoryId]);
 
   const formatTitle = (str) => {
     if (str === "nomadedigital") {
@@ -34,12 +43,12 @@ function ItemListContainer() {
 
   return (
     <div className="body-container">
-      <h2 className='title'>{category ? "Guia: " + formatTitle(category) : "Conoce todas nuestras guias"}</h2>
+      <h2 className='title'>{categoryId ? "Guia: " + formatTitle(categoryId) : "Conoce todas nuestras guias"}</h2>
       <div className='cards'>
-        <ItemList products={products}/>
+        <ItemList products={products} />
       </div>
     </div>
-  )
+  );
 }
 
 export default ItemListContainer;
